b77ddd0542e689862ce9bae6edda26b2e373d8ab
Vlad96Kir <130667144+Vlad96Kir@users.noreply.github.com>
2023-11-15 18:26:36 +0200

    Add files via upload

---
+++ pythonProject24/path_db/add_data.py
@@ -0,0 +1,21 @@
+import data_processing
+from db_models import Author, Quote
+import connect
+
+with open('/Users/kyryl/PycharmProjects/pythonProject24/hw9_spider/json/authors.json',
+          'r', encoding='utf-8') as authors:
+    authors_data = json.load(authors)
+
+for author_data in authors_data:
+    author = Author(**author_data)
+    author.save()
+
+with open('/Users/kyryl/PycharmProjects/pythonProject24/hw9_spider/json/quotes.json',
+          'r', encoding='utf-8') as quotes:
+    quotes_data = json.load(quotes)
+
+for quote_data in quotes_data:
+    author = Author.objects.get(fullname=quote_data['author'])
+
+    quote = Quote(**quote_data)
+    quote.save()
